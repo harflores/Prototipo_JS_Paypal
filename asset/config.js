784b821b5220685bb87fd882710bfc30b0f81503
harflores <49909627+harflores@users.noreply.github.com>
2022-07-29 17:42:12 -0400

    limpieza

--- asset/config.js
+++
@@ -1,6 +0,0 @@
-// DECLARACION DE VARIABLES.
-
-// PAYPAL
-let paypalClientId = "AXEHpDGN86Hi66RlfLCPgXreJVV3ZirXAaPMi8hvNibMh-d3IKZqO8UFu41hZTTbz6l6CqcRxU3Zv5O2" ;
-let paypalSecret = "ELAYLgz5sr_rk2ArCajxSwOFH40DGadtu71rsdwl8MFD5hrzcaRMfzTZzB5mRWAc94oq5Er8Z4YLh3dX";
-let paypalMode = "";
--- asset/paypal.js
+++ asset/paypal.js
@@ -1,71 +1,54 @@
+function cuestionario(){
+            const element ='<h3>Aqui comienza el cuestionario :)</h3>'; 
+            return element
+          }
 
-const fundingSources = [
-     paypal.FUNDING.CARD
-   ]
-
- for (const fundingSource of fundingSources) {
-   const paypalButtonsComponent = paypal.Buttons({
-     fundingSource: fundingSource,
-
-     // optional styling for buttons
-     // https://developer.paypal.com/docs/checkout/standard/customize/buttons-style-guide/
-     style: {
-       shape: 'pill',
-       height: 40,
-     },
-
-     // set up the transaction
-     createOrder: (data, actions) => {
-
-      
-       // pass in any options from the v2 orders create call:
-       // https://developer.paypal.com/api/orders/v2/#orders-create-request-body
-       const createOrderPayload = {
-           application_context:{
-                shipping_preference: "NO_SHIPPING"
-           },
-           address:{
+          function initPayPalButton() {
+            const fundingSources = [
+           paypal.FUNDING.CARD
+         ]
+            paypal.Buttons({
+              style: {
+                shape: 'rect',
+                color: 'black',
+                layout: 'vertical',
+                label: 'checkout',
                 
-           },
-         purchase_units: [
-           {
-             amount: {
-               value: '5.00',
-             },
-           },
-         ],
-       }
-
-       return actions.order.create(createOrderPayload)
-     },
-
-     // finalize the transaction
-     onApprove: (data, actions) => {
-       const captureOrderHandler = (details) => {
-         const payerName = details.payer.name.given_name
-         // AQUI VA EL INICIO DE LAS PREGUNTAS
-         alert(details)
-         console.log('Transaction completed!')
-         
-         
-       }
-
-       return actions.order.capture().then(captureOrderHandler)
-     },
-
-     // handle unrecoverable errors
-     onError: (err) => {
-       alert(err)
-     },
-   })
-
-   if (paypalButtonsComponent.isEligible()) {
-     paypalButtonsComponent
-       .render('#paypal-button-container')
-       .catch((err) => {
-         console.error('PayPal Buttons failed to render')
-       })
-   } else {
-     console.log('The funding source is ineligible')
-   }
- }
+              },
+              
+      
+              createOrder: function(data, actions) {
+                
+                return actions.order.create({
+                  application_context:{
+                      shipping_preference: "NO_SHIPPING"
+                 },
+                 address:{
+                      
+                 },
+                  purchase_units: [{"amount":{"currency_code":"EUR","value":5}}]
+                });
+              },
+      
+              onApprove: function(data, actions) {
+                return actions.order.capture().then(function(orderData) {
+                  
+                  // Full available details
+                  console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+      
+                  // Show a success message within this page, e.g.
+                  const element = document.getElementById('paypal-button-container');
+                  element.innerHTML = '';
+                  element.innerHTML = cuestionario();
+      
+                  // Or go to another URL:  actions.redirect('thank_you.html');
+                  
+                });
+              },
+      
+              onError: function(err) {
+                console.log(err);
+              }
+            }).render('#paypal-button-container');
+          }
+          initPayPalButton();
